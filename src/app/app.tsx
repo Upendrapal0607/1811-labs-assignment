27cf2ed58d3c651f1c8ff941038c58e2042056f4
Upendra Pal <112810287+Upendrapal0607@users.noreply.github.com>
2024-07-13 12:43:17 +0530

    Update app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { getCookie } from "./lib/utils";
+
 export const App = () => {
   const navigate = useRouter();
   const token = getCookie("sb-vxysqlojrtxmdzgxcqda-auth-token.0"); 
@@ -10,17 +11,17 @@ export const App = () => {
   };
 
   return (
-    <div className="w-full min-h-screen bg-[url('/Image/background-image.png')] text-white bg-cover m-0 p-0">
-      <div className="flex gap-8 flex-col w-[185px] h-[130px] pt-[7%] ml-[43%]  text-center font-pacifico text-3xl space-y-2">
+    <div className="w-full min-h-screen  bg-[url('/Image/background-image.png')] bg-cover bg-center text-white m-0 p-0 flex flex-col items-center justify-between">
+      <div className="flex flex-col items-center w-full md:w-auto text-center font-pacifico text-3xl space-y-6 mt-[150px] md:mt-[40px] sm:mt-[120px]">
         <h3>Pickup line</h3>
         <h3>Generator</h3>
       </div>
-      <div className="mt-[25%] ml-[40%]">
+      <div className="mt-8 md:mt-16">
         <button
-          className="flex items-center justify-center px-8 py-4 bg-[#ff2157] text-white  rounded-full text-xl shadow-lg hover:bg-[#ff4071] transition duration-300"
+          className="flex items-center justify-center mb-[350px] lg:mb-[250px] px-8 py-4 bg-[#ff2157] text-white rounded-full text-xl shadow-lg hover:bg-[#ff4071] transition duration-300"
           onClick={handleLogin}
         >
-          <span>🤍</span> <p>Generat one for me</p> <span>🤍</span>
+          <span>🤍</span> <p>Generate one for me</p> <span>🤍</span>
         </button>
       </div>
     </div>
