54dea5c1f916fedc9f3f981a0cc4cd038805e4fb
Upendra Pal <up06072000@gmail.com>
2024-07-13 17:56:44 +0530

    update responsive

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useRouter } from "next/navigation";
 import { getCookie } from "./lib/utils";
 
@@ -11,17 +11,18 @@ export const App = () => {
   };
 
   return (
-    <div className="w-full min-h-screen  bg-[url('/Image/background-image.png')] bg-cover bg-center text-white m-0 p-0 flex flex-col items-center justify-between">
-      <div className="flex flex-col items-center w-full md:w-auto text-center font-pacifico text-3xl space-y-6 mt-[150px] md:mt-[40px] sm:mt-[120px]">
+    <div className="w-full min-h-screen bg-[url('/Image/background-image.png')] bg-center sm:bg-cover md:bg-contain text-white flex flex-col items-center justify-between p-4">
+      <div className="flex flex-col items-center w-full text-center font-pacifico text-3xl space-y-4 mt-32 md:mt-16 sm:mt-24">
         <h3>Pickup line</h3>
         <h3>Generator</h3>
       </div>
+      <div className="flex-grow"></div>
       <div className="mt-8 md:mt-16">
         <button
-          className="flex items-center justify-center mb-[350px] lg:mb-[250px] px-8 py-4 bg-[#ff2157] text-white rounded-full text-xl shadow-lg hover:bg-[#ff4071] transition duration-300"
+          className="flex items-center justify-center mb-20 md:mb-28 lg:mb-36 px-6 py-3 bg-[#ff2157] text-white rounded-full text-lg md:text-xl shadow-lg hover:bg-[#ff4071] transition duration-300"
           onClick={handleLogin}
         >
-          <span>🤍</span> <p>Generate one for me</p> <span>🤍</span>
+          <span>🤍</span> <p className="mx-2">Generate one for me</p> <span>🤍</span>
         </button>
       </div>
     </div>
